ee3ed51fc8f5df7975590e4eb1f787eabbec2a15
Dave Holdsworth <daholdsworth@gmail.com>
2017-05-15 20:16:12 +0100

    evaluation tests

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,17 +3,19 @@ package main
 import (
 	"fmt"
 	"strconv"
+
+	"github.com/londondave/rampage/pkg/hand"
 )
 
 func main() {
-	for n := 0; n < 10; n++ {
-		cards := Shuffle(17)
+	for n := 0; n < 1; n++ {
+		cards := hand.Shuffle(17)
 		for i := 0; i < 12; i = i + 2 {
 			for i := 12; i < 17; i++ {
-				fmt.Print(cardName(cards[i]), "  ")
+				fmt.Print(hand.CardName(cards[i]), "  ")
 			}
-			fmt.Print(cardName(cards[i]), "  ", cardName(cards[i+1]), "    ")
-			handValue, mask := Evaluate(cards[i:i+2], cards[12:17])
+			fmt.Print(hand.CardName(cards[i]), "  ", hand.CardName(cards[i+1]), "    ")
+			handValue, mask := hand.Evaluate(cards[i:i+2], cards[12:17])
 			fmt.Println(strconv.FormatInt(int64(handValue), 16), mask)
 		}
 	}
--- pkg/hand/hand.go
+++ pkg/hand/hand.go
@@ -6,7 +6,7 @@ import (
 	"math/big"
 )
 
-func cardName(card int) string {
+func CardName(card int) string {
 	suitName := "chds"
 	rankName := "A23456789TJQK"
 	suit := card / 13
@@ -14,7 +14,7 @@ func cardName(card int) string {
 	return rankName[rank:rank+1] + suitName[suit:suit+1]
 }
 
-func shuffle(numCards int) []int {
+func Shuffle(numCards int) []int {
 
 	deck := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12,
 		13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25,
@@ -35,7 +35,7 @@ func shuffle(numCards int) []int {
 
 }
 
-func evaluate(hole []int, board []int) (evaluation int, mask []int) {
+func Evaluate(hole []int, board []int) (evaluation int, mask []int) {
 	var suits [4]int
 	var ranks [14]int
 	var order [53]int
